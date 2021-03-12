a9cc2591014a6309080532537e7ff6a978abb4e5
Serda Simus <iamserda@gmail.com>
2021-03-11 19:03:01 -0500

    updated js file, updated speed

--- index.js
+++ index.js
@@ -12,36 +12,40 @@
 */
 
 async function getPokemon() {
-    const fetchPokemonResponse = await fetch('pokemon.json');
-    const data = await fetchPokemonResponse.json();
+  const fetchPokemonResponse = await fetch("pokemon.json");
+  const data = await fetchPokemonResponse.json();
 
-    return data;
-
+  return data;
 }
 
 function createApp() {
-    const h1 = document.createElement('h1');
-    h1.innerText = "PokéDeck 8000"
-    h1.classList.add('app-title');
-    
-    const displayContainer = document.createElement('div');
-    
-    displayContainer.classList.add('grid', 'app')
-    displayContainer.id = 'app';
-    document.body.prepend(displayContainer);
-    document.body.prepend(h1);
-    // const nextButton = document.createElement('a');
-    // const prevButton = document.createElement('a');
-    // nextButton.href = '#'
-    // prevButton.href = '#'
-    // nextButton.innerText = 'next'
-    // prevButton.innerText = 'prev'
+  const h1 = document.createElement("h1");
+  h1.innerText = "PokéDeck 8000";
+  h1.classList.add("app-title");
+
+  const displayContainer = document.createElement("div");
+
+  displayContainer.classList.add("grid", "app");
+  displayContainer.id = "app";
+  document.body.prepend(displayContainer);
+  document.body.prepend(h1);
+  // const nextButton = document.createElement('a');
+  // const prevButton = document.createElement('a');
+  // nextButton.href = '#'
+  // prevButton.href = '#'
+  // nextButton.innerText = 'next'
+  // prevButton.innerText = 'prev'
 }
 
 async function displayPokemon(item) {
-    const { id, name: { english },type, base: { HP, Attack, Defense, Speed } } = item;
-    const pokemonBox = document.createElement('div');
-    const pokeHTML = `
+  const {
+    id,
+    name: { english },
+    type,
+    base: { HP, Attack, Defense, Speed },
+  } = item;
+  const pokemonBox = document.createElement("div");
+  const pokeHTML = `
         <span class='info id'>${id}</span>
         <span class='info name'>${english}</span>
         <span class='info type'>Type:<br> ${type}</span>
@@ -49,27 +53,25 @@ async function displayPokemon(item) {
         <span class='info attack'>Attack:<br> ${Attack}</span>
         <span class='info defense'>Defense:<br>${Defense}</span>
         <span class='info speed'>Speed:<br> ${Speed}</span>
-    `
-    pokemonBox.classList.add('pokemon');
-    pokemonBox.innerHTML = pokeHTML;
-    const app = document.getElementById('app');
-    app.prepend(pokemonBox);
+    `;
+  pokemonBox.classList.add("pokemon");
+  pokemonBox.innerHTML = pokeHTML;
+  const app = document.getElementById("app");
+  app.prepend(pokemonBox);
 }
 
 // IIFE are a life-line to testing code as you implement them!
 // https://developer.mozilla.org/en-US/docs/Glossary/IIFE
-(
-    async () => {
-        createApp();
-        const data = await getPokemon();
-        let counter = 0;
-        
-        // iterate into an array using time.
-        let intervalID = setInterval(()=>{
-            displayPokemon(data[counter]);
-            counter++}
-            , 500);
-        
-        setTimeout(clearInterval, 40500, intervalID);
-    }
-)();
+(async () => {
+  createApp();
+  const data = await getPokemon();
+  let counter = 0;
+
+  // iterate into an array using time.
+  let intervalID = setInterval(() => {
+    displayPokemon(data[counter]);
+    counter++;
+  }, 250);
+
+  setTimeout(clearInterval, 500000, intervalID);
+})();
